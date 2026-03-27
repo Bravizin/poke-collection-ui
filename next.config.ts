fe8434d9fdad24f339a31c1bc447b1c57ba02719
bravizin <leo_rse@hotmail.com>
2026-03-27 18:31:48 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/poke-collection-ui', // Necessário para imagens/scripts não darem 404 no Github pages!
   images: {
     unoptimized: true, // Required for static export on Github Pages
   },
